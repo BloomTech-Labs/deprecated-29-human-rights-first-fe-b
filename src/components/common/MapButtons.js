4010981226aa629fd202bc6e5eac6286e90873d8
Sam Brown <sambrown@Sams-MacBook-Pro.local>
2020-11-12 09:49:17 -0700

    removes the warning overlay

--- src/components/common/MapButtons.js
+++ src/components/common/MapButtons.js
@@ -122,7 +122,7 @@ const MapButtons = ({ scrollEnabled, map, usZips }) => {
         Hide Map Options 🔼
       </Button>
 
-      <div id="disappear">
+      {/* <div id="disappear">
         <Result
           status="warning"
           icon={<WarningFilled style={{ color: 'red', fontSize: '6rem' }} />}
@@ -195,7 +195,7 @@ const MapButtons = ({ scrollEnabled, map, usZips }) => {
             </Button>
           }
         />
-      </div>
+      </div> */}
     </div>
   );
 };
